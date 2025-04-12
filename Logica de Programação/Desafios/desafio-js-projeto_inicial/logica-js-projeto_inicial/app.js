39df79222407c8da64b29f7f5593bc561f4ed760
Carlos Eduardo <carloseduardodev99@gmail.com>
2025-04-12 12:21:12 -0300

    Desafio JS

---
+++ Logica de Programação/Desafios/desafio-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,11 @@
+alert("Boas Vindas ao nosso Site!");
+let nome = "lua";
+let idade = 25;
+let numerodevendas = 50;
+let saldodisponivel = 1000;
+alert("ERRO, preencha todos os campos");
+let mensagemdeerro = "ERRO, preencha todos os campos";
+alert(mensagemdeerro);
+let nome = prompt("Digite seu nome: ");
+let idade = prompt("Digite aqui sua idade: ");
+if(idade>=18){ alert("Pode tirar habilitação)"; } else { alert('Não pode tirar habilitação'); }
