2c1609d30eaf397f643e60362b8bb42d007b1787
Carlos Eduardo <carloseduardodev99@gmail.com>
2025-04-12 11:35:48 -0300

    Exercicios

---
+++ Logica de Programação/Exercicios/logica-js-projeto_inicial/app.js
@@ -0,0 +1,6 @@
+alert('Bem Vindo ao jogo do numero secreto!');
+let numeroSecreto = 9;
+let chute = prompt('Escolha um numero entre 1 e 10!')
+if(chute == numeroSecreto) {
+    console.log('Voce descobriu o numero secreto (9)');
+}
